6a8e0ca1ab57421fdbbf5334bbb1ddad747732c2
Haofan Wang <95482803+Airwhf@users.noreply.github.com>
2023-07-25 14:26:08 +0800

    Delete clip2china.py

--- clip2china.py
+++
@@ -1,8 +0,0 @@
-import xarray as xr
-
-
-dataset = xr.open_dataset('Input/SA/landscan-global-2017_nodata.nc')
-dataset = dataset.sel(lon=slice(70, 150), lat=slice(70, 0))
-
-# print(dataset)
-dataset.to_netcdf('Input/SA/china_population_2017.nc')
