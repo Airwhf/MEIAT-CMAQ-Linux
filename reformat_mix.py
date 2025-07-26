f49308bda87d82692082e22b99a8a677ed3609e7
Haofan Wang <95482803+Airwhf@users.noreply.github.com>
2025-07-26 21:20:49 +0800

    Create reformat_mix.py

---
+++ reformat_mix.py
@@ -0,0 +1,8 @@
+from src_public import emis_mix
+
+emis_mix(
+    '/Volumes/project_new/data_pkucn/mix_emis_2017/mix_emis_2017',
+    '/Volumes/project_new/data_pkucn/mix_emis_2017/mix_emis_2017_meiat',
+    2017,
+    2,
+)
